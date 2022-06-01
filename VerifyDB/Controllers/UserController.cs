4168f277c483c0ccadc6bed52081bc680fb40f13
T1maty <timamy666@gmail.com>
2022-06-01 23:55:52 +0300

    Controller Add

---
+++ VerifyDB/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using VerifyDB.Data;
+using VerifyDB.Models;
+
+namespace VerifyDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if (_context.Users.Any( u => u.Email == request.Email))
+            {
+                return BadRequest("User already exists.");
+            }
+            CreatePasswordHash(request.Password, 
+                out byte[] passwordHash, 
+                out byte[] passwordSalt);
+
+        }
+        private void  CreatePasswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)); 
+            }
+        }
+    }
+}
