5dbfdb968f149b4512ee3553b3044e07d2ce2006
T1maty <timamy666@gmail.com>
2022-06-14 21:47:07 +0300

    Add Serilog

--- VerifyDB/Program.cs
+++ VerifyDB/Program.cs
@@ -1,13 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using VerifyDB.Data;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
