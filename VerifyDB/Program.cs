4adb34155eac88f55425f28c1ddabf4c7e56659f
Timaty <101876583+T1maty@users.noreply.github.com>
2022-06-22 21:13:54 +0300

    pizdec

--- VerifyDB/Program.cs
+++ VerifyDB/Program.cs
@@ -4,15 +4,28 @@ using VerifyDB.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 builder.Services.AddAutoMapper(c => c.AddProfile<MapperProfile>());
 
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
 
+
+
 builder.Services.AddDbContext<DataContext>(optionsBuilder => optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString(nameof(DataContext))));
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHealthChecks("/health");
+    endpoints.MapControllers();
+});
 
 if (app.Environment.IsDevelopment())
 {
